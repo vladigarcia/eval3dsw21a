42066750987b6dbc8ce304a86c65e2eb0a45e6f3
vladigarcia <144178838+vladigarcia@users.noreply.github.com>
2023-10-22 13:59:12 -0600

    cambiando el estilo de snackbar

--- lib/snackbar.dart
+++ lib/snackbar.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Snackbar extends StatelessWidget {
-
   void _mostrarSnackbar(BuildContext context) {
     final snackBar = SnackBar(
-      content: Text('No podemos evitar que los problemas nos encuentren, pero podemos elegir cómo los enfrentamos'),
+      content: Text(
+        'No podemos evitar que los problemas nos encuentren, pero podemos elegir cómo los enfrentamos',
+        style: TextStyle(fontSize: 16, color: Colors.white),
+      ),
       backgroundColor: Colors.blue, // Cambia el color de fondo del Snackbar
+      elevation: 6, // Agrega una sombra
+      behavior: SnackBarBehavior.floating,
       action: SnackBarAction(
         label: 'Deshacer',
+        textColor: Colors.yellow,
         onPressed: () {
-
+          // Agrega la lógica de deshacer aquí
         },
       ),
     );
@@ -22,12 +27,18 @@ class Snackbar extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Snackbar'),
+        backgroundColor: Colors.blue, // Cambia el color de fondo de la AppBar
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.blue, // Cambia el color del botón
+                onPrimary: Colors.white, // Cambia el color del texto del botón
+                elevation: 5, // Agrega una sombra al botón
+              ),
               onPressed: () {
                 // Llama a la función para mostrar el Snackbar
                 _mostrarSnackbar(context);
